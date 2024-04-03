a18e7b2b0a11bf1b9ec5a208d9b2c5bfc3cd714a
Francesca Correa <francescacciavaglia@gmail.com>
2024-04-03 16:00:34 +0200

    fix order of functions and add spacing

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import menuArray from "/data.js"
 
-const orderDetailsEl = document.getElementById('order-details')
-const orderTotalEl = document.getElementById('order-total')
 const paymentModal = document.getElementById('payment-modal')
 const paymentForm = document.getElementById('payment-form')
 const orderDetailsArr = []
 const itemsPrices = []
 
 document.addEventListener('click', function(e){
+
   if (e.target.dataset.add){
     getSelectedItem(Number(e.target.dataset.add))
   } else if (e.target.id === 'order-btn'){
@@ -18,16 +17,51 @@ document.addEventListener('click', function(e){
 })
 
 paymentForm.addEventListener('submit', function(e) {
+
   e.preventDefault()
+
   paymentModal.style.display = 'none'
-  orderTotalEl.style.display = 'none'
   document.getElementById('order-section').style.display = 'none'
   document.getElementById('completed-msg').style.display = 'block'
 })
 
+function getSelectedItem(itemId){
+
+  const selectedItemObj = menuArray.filter( menuItem => menuItem.id === itemId )[0]
+
+  orderDetailsArr.push(selectedItemObj)
+  itemsPrices.push(selectedItemObj.price)
+
+  getOrderDetailsHtml(orderDetailsArr)
+  orderTotal(itemsPrices)
+}
+
+function renderPaymentModal() {
+
+  if (orderDetailsArr.length > 0){
+    paymentModal.style.display = 'inline'
+  }
+}
+
+function removeSelectedItem(itemId){
+
+  const selectedItem = menuArray.filter( menuItem => menuItem.id === itemId)[0]
+  const itemIndex = orderDetailsArr.indexOf(selectedItem)
+  const itemPriceIndex = itemsPrices.indexOf(selectedItem.price)
+
+  orderDetailsArr.splice(itemIndex, 1)
+  itemsPrices.splice(itemPriceIndex, 1)
+
+  getOrderDetailsHtml(orderDetailsArr)
+  orderTotal(itemsPrices)
+}
+
 function getItemsHtml() {
+
   return menuArray.map(menu => {
+
     const { name, ingredients, id, price, emoji } = menu
+
     return `
     <div class="item-card">
       <div class="item-first">
@@ -44,56 +78,37 @@ function getItemsHtml() {
 }
 
 function renderItemsList() {
+
   const itemsContainer = document.getElementById('items-container')
+
   itemsContainer.innerHTML = getItemsHtml()
 }
 
 renderItemsList()
 
-function getSelectedItem(itemId){
-  const selectedItemObj = menuArray.filter( menuItem => menuItem.id === itemId )[0]
-  orderDetailsArr.push(selectedItemObj)
-  itemsPrices.push(selectedItemObj.price)
-  getOrderDetailsHtml(orderDetailsArr)
-  orderTotal(itemsPrices)
-}
-
 function getOrderDetailsHtml(array) {
 
-  const orderDetailsHtml = array.map(function(item){
-    return `
+  const orderDetailsHtml = array.map( item => `
     <div class="item-details">
-      <p>${item.name} <span class='remove-order-item' id="remove-item" data-remove='${item.id}'>remove</span></p>
+      <p>
+      ${item.name}
+      <span class='remove-order-item' id="remove-item" data-remove='${item.id}'>remove</span>
+      </p>
       <span class='order-item-price'>$${item.price}</span>
     </div>`
-  }).join('')
+  ).join('')
 
   renderOrderDetails(orderDetailsHtml)
 }
 
 function renderOrderDetails(html){
-  orderDetailsEl.innerHTML = html
+
+  document.getElementById('order-details').innerHTML = html
 }
 
 function orderTotal(array){
-  const orderTotal = array.reduce(function(total, current){
-    return total + current
-  }, 0)
-  orderTotalEl.textContent = '$' + orderTotal
-}
 
-function renderPaymentModal() {
-  if (orderDetailsArr.length > 0){
-    paymentModal.style.display = 'inline'
-  }
-}
+  const orderTotal = array.reduce( (total, current) => total + current, 0)
 
-function removeSelectedItem(itemId){
-  const selectedItem = menuArray.filter( menuItem => menuItem.id === itemId)[0]
-  const itemIndex = orderDetailsArr.indexOf(selectedItem)
-  const itemPriceIndex = itemsPrices.indexOf(selectedItem.price)
-  orderDetailsArr.splice(itemIndex, 1)
-  itemsPrices.splice(itemPriceIndex, 1)
-  getOrderDetailsHtml(orderDetailsArr)
-  orderTotal(itemsPrices)
+  document.getElementById('order-total').textContent = '$' + orderTotal
 }
